f8d5852ad3f5985b92868e531da2b697b388d957
julien <julien.mazet@gmail.com>
2018-04-06 13:05:02 +0200

    Use the selected list to navigate to the shop view

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+
 import {ListService} from '../list.service';
 
 @Component({
@@ -9,8 +11,29 @@ import {ListService} from '../list.service';
 export class HomeComponent implements OnInit {
 
   listsName: String[];
+  selectedListName: String;
 
-  constructor(private listService: ListService) { }
+  constructor(private listService: ListService, private router: Router) { }
+
+
+  /**
+   * If a list is selected, navigate to the shop view for this list.
+   * Else display a warning in the console.
+   */
+  navigateToShop(){
+    if(! this.selectedListName){
+      console.warn("No list selected yet!")
+      return;
+    }
+
+    let id = this.listService.getListByName(this.selectedListName).id
+
+    this.router.navigateByUrl("/shop/"+id).then(nav => {
+      console.log(nav);
+    }).catch(err => {
+      console.error(err);
+    });
+  }
 
   ngOnInit() {
     this.listsName = this.listService.getAllListsNames();
